b0068c998108809e9cf38995d726646cecaa636f
conner <Conner@reitmeier.org>
2021-03-20 22:08:07 -0500

    orbital tool library

---
+++ csOrbitTools/OrbitTools/Orbit/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2003-2020 Michael F. Henry
+// Version 02/2020
+//
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Zeptomoby.OrbitTools.Orbit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zeptomoby.com")]
+[assembly: AssemblyProduct("OrbitTools Library - Public Edition")]
+[assembly: AssemblyCopyright("Copyright © 2003-2021 Michael F. Henry")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyVersion    ("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.4.1.0")]
